68b771cfc4b8198f0966113092f4b33dd75b7807
Tim <sown-lagos-chao-elena-pont>
2019-05-24 10:48:25 +0200

    Take camera images and show

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from beamngpy import BeamNGpy, Scenario, Vehicle
 from beamngpy.sensors import Camera
 from threading import Thread
 from time import sleep
+from PIL import Image
 
 random.seed(1700)
 
@@ -17,16 +18,13 @@ thread.start()
 
 # Instantiate BeamNGpy instance running the simulator from the given path,
 # communicating over localhost:64256
-bng = BeamNGpy('localhost', 64256, home='/path/to/bng/research')
-
-bng.set_deterministic()
-bng.set_steps_per_second(60)
+bng = BeamNGpy('localhost', 64256, home='C:\\Users\\Tim\\Documents\\research\\trunk')
 
 # Create a scenario in west_coast_usa called 'example'
 scenario = Scenario('west_coast_usa', 'example')
 # Create an ETK800 with the licence plate 'PYTHON'
-vehicle = Vehicle('host_vehicle', model='etk800', licence='RL FTW')
-front_camera = Camera(pos=(-0.3, 1, 1.0), direction=(0, 1, 0), fov=120, resolution=(100, 100), colour=True)
+vehicle = Vehicle('ego_vehicle', model='etk800', licence='RL FTW', color='Red')
+front_camera = Camera(pos=(-0.3, 1, 1.0), direction=(0, 1, 0), fov=120, resolution=(1024, 1024), colour=True)
 vehicle.attach_sensor("monocular camera", front_camera)
 
 # Add it to our scenario at this position and rotation
@@ -36,16 +34,24 @@ scenario.make(bng)
 
 # Launch BeamNG.research
 bng.open()
+
+bng.set_deterministic()
+bng.set_steps_per_second(60)
 # Load and start our scenario
 bng.load_scenario(scenario)
 bng.start_scenario()
+bng.pause()
+bng.hide_hud()
 
-while True:
-    throttle = random.uniform(0.0, 1.0)
-    steering = random.uniform(-1.0, 1.0)
-    brake = random.choice([0, 0, 0, 1])
-    vehicle.control(throttle=throttle, steering=steering, brake=brake)
 
-    bng.step(20)
-    sensors = bng.poll_sensors(vehicle)
-    print(sensors)
+throttle = random.uniform(0.0, 1.0)
+steering = random.uniform(-1.0, 1.0)
+brake = random.choice([0, 0, 0, 1])
+vehicle.control(throttle=throttle, steering=steering, brake=brake)
+
+bng.step(20)
+sensors = bng.poll_sensors(vehicle)
+print(sensors)
+sensors['monocular camera']['colour'].show()
+print(type(sensors['monocular camera']['colour']))
+
