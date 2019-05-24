0f1c15c07b598eab6b9d6fc439208c6510b9c445
Tim <sown-lagos-chao-elena-pont>
2019-05-24 18:39:25 +0200

    Train convnet

---
+++ datasets/train.py
@@ -0,0 +1,8 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import numpy as np
+from keras import backend as K
+
